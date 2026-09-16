e416b4f86daa916e562f520df29d94bdce14702d
Krishna Singh <krishnasinghks1225@gmail.com>
2026-09-16 22:58:15 +0530

    Time: 34 ms (60.65%) | Memory: 62 MB (74.84%) - LeetSync

--- 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,44 +1,36 @@
 class Solution {
-
-    int n;
-    int[][][] dp;
     static int MOD = 1000000007;
-
     public int numberOfSets(int n, int k) {
-        this.n = n;
-        dp = new int[2][n][k+1];
+        return numberOfSet(n, k);
+    }
 
-        for(int[][] d: dp) {
-            for(int[] d1: d) {
-                Arrays.fill(d1, -1);
+    public int numberOfSet(int n, int k) {
+        int[][][] dp = new int[2][n+1][k+1];
+
+        for(int i = 0; i < n; i++) {
+            for(int state = 0; state < 2; state++) {
+                dp[state][i][0] = 1;
             }
         }
 
-        return numberOfSet(0, k, 0);
-    }
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = 1; j <= k; j++) {
+                for(int state = 0; state <= 1; state++) {
+                    int ans = 0;
+                    if(state == 1) {
+                        ans = (ans + dp[0][i][j-1]) % MOD;
+                    }
+                    else {
+                        ans = (ans + dp[1][i+1][j]) % MOD;
+                    }
 
-    public int numberOfSet(int i, int k, int state) {
-        if(k == 0) {
-            return 1;
+                    //skip
+                    ans = (ans + dp[state][i+1][j]) % MOD;
+                    dp[state][i][j] = ans;
+                }
+            }
         }
 
-        if(i == n) {
-            return 0;
-        }
-
-        if(dp[state][i][k] != -1) {
-            return dp[state][i][k];
-        }
-
-        int ans = 0;
-        if(state == 1) {
-            ans = (ans + numberOfSet(i, k-1, 0)) % MOD;
-        }
-        else {
-           ans = (ans + numberOfSet(i+1, k, 1)) % MOD;
-        }
-
-        int skip = numberOfSet(i+1, k, state);
-        return dp[state][i][k] = (ans + skip) % MOD;
+        return dp[0][0][k];
     }
 }
